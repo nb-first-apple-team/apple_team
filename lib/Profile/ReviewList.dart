b1fa62d25323eaf92854d761f161a2693f26c8d5
hamham1210 <thwn1012@gmail.com>
2023-07-11 17:09:29 +0900

    test

--- lib/Profile/ReviewList.dart
+++ lib/Profile/ReviewList.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 
 import 'ProfileList.dart';
 
-class ReviewPage extends StatelessWidget {
+class ReviewPage extends StatefulWidget {
   const ReviewPage({super.key});
 
+  @override
+  State<ReviewPage> createState() => _ReviewPageState();
+}
+
+class _ReviewPageState extends State<ReviewPage> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
