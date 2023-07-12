68b2b78a58c64d32bc6ab911eebc8ef151424697
4seokhyeon <tktjrgus@naver.com>
2023-07-12 15:18:38 +0900

    Top commit

--- lib/Profile/ReviewList.dart
+++ lib/Profile/ReviewList.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-import 'ProfileList.dart';
-
-class ReviewPage extends StatelessWidget {
-  const ReviewPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) => ListTile(),
-    );
-  }
-}
