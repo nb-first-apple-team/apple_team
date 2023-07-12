402bf0b6baea00aadceff6e63355372e24cb3a4b
somethinginthebox <ha862888@gmail.com>
2023-07-12 09:30:59 +0900

    홈화면 만들기

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,27 +1,131 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 import 'Contetnt.dart';
 
+void main() {
+  runApp(const HomePage());
+}
+
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ContentPage()),
-              );
-            },
-            child: Text('콘텐트페이지이동'),
-          )
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.green[400],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 60,
+                  bottom: 200,
+                ),
+                child: Row(
+                  children: [
+                    Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/976px-Apple_logo_black.svg.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                    const SizedBox(width: 10),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 30,
+                          left: 50,
+                          top: 50,
+                        ),
+                        child: Text(
+                          'Welcome\n ,apple\n Team',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: SizedBox(
+                  width: 350,
+                  height: 280,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Transform.translate(
+                      offset: Offset(0, -150), // Move the box 20 pixels up
+                      child: Container(
+                        padding: const EdgeInsets.all(18),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Team Content\n',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              'Team Profile',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 70),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ContentPage()),
+                                );
+// 버튼 클릭 시 동작
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.green[800],
+                                minimumSize: Size(double.infinity, 60),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80),
+                                ),
+                              ),
+                              child: Text(
+                                '팀소개 Go',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
